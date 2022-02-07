4a3e747b03a982c4b11fe015983b8ee4394154e3
heyazoo1007 <heyazoo1007@gmail.com>
2022-02-07 15:07:05 +0900

    DIP,OCP 만족하게 코드 수정

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,14 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); //구현 클래스 선언
     //memorymemberrepo는 추상화,구현 모두애 의존(알고있다)
+    private final MemberRepository memberRepository ; //구현 클래스 선언
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
@@ -12,6 +18,7 @@ public class MemberServiceImpl implements MemberService{
 
     @Override
     public Member findMember(Long memberId) {
+
         return memberRepository.findById(memberId);
     }
 }
